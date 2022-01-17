eb889c33ae731769c44d49faa8e07b7a88b7a810
Brandon G. Merritt <brandongmerritt@gmail.com>
2022-01-16 20:47:08 -0600

    Posts component .1

---
+++ src/Components/Posts/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+
+function Posts() {
+    return (
+    <div>
+        <h2>Posts</h2>
+        <p>If you can see this it means it's working!</p>
+    </div>
+    )
+}
+
+export default Posts;

a2d8e6bd29e55d9e1d23c80b187fc8a345ec377c
Brandon G. Merritt <brandongmerritt@gmail.com>
2022-02-13 12:50:32 -0600

    Toxicity and Follow Up

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,7 +1,6 @@
 import defaults from "nodemon/lib/config/defaults";
 import React from "react";
 import './index.css';
-// import {gsap} from "gsap";
 
 function Blog() {
   return (
@@ -14,7 +13,7 @@ function Blog() {
               <p>Here we go again, the endless quest for the job continues to be hunted down.
                 There is no telling how far we will go for greatness; some spend thousands of dollars to figure out and some more than others.
                 I went through an entire undergrad only to get another set of schooling under my belt. Graphic design is great and all, but I can't support an entire family with GD money.
-                There was a time where I thought I'd be working at big time companies doing big time things; sadly not yet. I know for a fact I am capable of creating award-winning content, so I need to start.
+                There was a time where I thought I'd be working at big time companies doing big time things; sadly, not yet. I know for a fact I am capable of creating award-winning content, so I need to start.
                 This is exactly why I went on to figure myself into a coding bootcamp and take on the ultimate task of getting a web dev job and use my graphic design background to aide my efforts.
                 Who doesnt want nice looking websites and functionality all in one?
               </p>
@@ -62,7 +61,7 @@ function Blog() {
                 We are creatures of habit - thus bad habits create a lesser life than what we would have if we only pushed a little bit more each day to our actual goals. Stop worrying about making someone else's dream happen and burning yourself out for your own dreams.
                 My dream is to be a website application developer through react and the only way I'll get there is to keep working on this website until it lands me the next step in my future. If it takes me every lunch period I have so be it; at the least I'm keeping myself up and at it.
                 We only fail when we give up on pushing through. As someone once said - 'lessons are taught until they are learned.' Will you keep learning or will you absorb the knowledge and figure it out for the next task that is adequate of your blood, sweat and tears?
-                Whether you can or can't you will. 
+                Whether you can or can't you will.
               </p>
             </div>
             <div className="orgo">
@@ -77,32 +76,32 @@ function Blog() {
                 It's slightly harsher with being the big blind in this exact situation; it'll be easier in a few years when it's not the case. No worries either way because this is what garnishes our character.
                 Perhahs.
               </p>
-              </div>
-              <div className="orgo">
-                <h2>One Step at a time</h2>
-                <p>
-                  I started an etsy account almost a week ago - business is not booming by any means at all. I keep reminding myself that good things take time and effort.
-                  I've put on some solid work for prints - 8.5x11 so we can easily order them until we make enough money for our printer. Then again, who knows if that'll ever happen.
-                  The easy part is etsy only charges twenty cents to post a product every few months so even if we never make a sale it's not like we're going broke giving it a solid go.
-                  I wish it was the other way anytime soon because I cannot wait for the next chapter of work to start. I don't anticipate it to be without problems; rather problems I actually care to solve.
-                  Graphic design is cool, but if you're working at a print shop or a newspaper you're not going to be too thrilled with the constant stream of bullshit that carry's through. I am grateful for the opportunities, but when will it pay off?
-                  Working the stock market is as bad if not worse than the lottery in some capacity - there's science to it but it's also a gamble that could be great today and worse tomorrow. 
-                  Here we all are looking to grind even farther for a furthered lifestyle. I only hope it pays off for you or me someday. 
-                </p>
-              </div>
-              <div className="orgo">
-                <h2>Someday</h2>
-                <p>Someday I'm going to be a senior developer at a really successful company and love my life regardless of how much stress and anguish that entails.
-                  Web dev is the absolute craziest, awesome idea I have the chance to learn daily and conquer regularly. Perhaps not now but sometime soon I'll catch my break and go beyond anything imagineable before.
-                  I'm already done with the core graphic design life - it's great for helping web dev but it's not helping my mental state work around problems that shouldn't be problems. Don't get me wrong I'm always on the frontline of attacking the problem first and foremost.
-                  There's nothing I can't figure out with enough time and effort - which I have plenty of - then again not really. You want all this done before tomorrow's end and you waited til 4:30 to tell me about it? Sure, but I can't gaurantee the highest level of quality with a half hour today and only some time tomorrow.
-                  You want me to stay late after work? - Hard no I have my own things I need to get done after work. Like getting a ticket out of this never ending hellscape.
-                  I'm not one to constantly complain - but have some boundaries for yourselves out there. Employers love to 'lovebomb' - a term only recently found out about. If you know it - great if not google it.
-                  You'll be waited on hand and foot only to be asked to break your back for a few dollars in comparison to the people running the show - when will employees have the upper hand in this toxicity?
-                  The people who make all the money for a single entity. Without employees there's zero employers. Unless you're like me and find it important to be able to self sufficently do every aspect of the business all on your own.
-                </p>
-              </div>
-              <div className="orgo">
+            </div>
+            <div className="orgo">
+              <h2>One Step at a time</h2>
+              <p>
+                I started an etsy account almost a week ago - business is not booming by any means at all. I keep reminding myself that good things take time and effort.
+                I've put on some solid work for prints - 8.5x11 so we can easily order them until we make enough money for our printer. Then again, who knows if that'll ever happen.
+                The easy part is etsy only charges twenty cents to post a product every few months so even if we never make a sale it's not like we're going broke giving it a solid go.
+                I wish it was the other way anytime soon because I cannot wait for the next chapter of work to start. I don't anticipate it to be without problems; rather problems I actually care to solve.
+                Graphic design is cool, but if you're working at a print shop or a newspaper you're not going to be too thrilled with the constant stream of bullshit that carry's through. I am grateful for the opportunities, but when will it pay off?
+                Working the stock market is as bad if not worse than the lottery in some capacity - there's science to it but it's also a gamble that could be great today and worse tomorrow.
+                Here we all are looking to grind even farther for a furthered lifestyle. I only hope it pays off for you or me someday.
+              </p>
+            </div>
+            <div className="orgo">
+              <h2>Someday</h2>
+              <p>Someday I'm going to be a senior developer at a really successful company and love my life regardless of how much stress and anguish that entails.
+                Web dev is the absolute craziest, awesome idea I have the chance to learn daily and conquer regularly. Perhaps not now but sometime soon I'll catch my break and go beyond anything imagineable before.
+                I'm already done with the core graphic design life - it's great for helping web dev but it's not helping my mental state work around problems that shouldn't be problems. Don't get me wrong I'm always on the frontline of attacking the problem first and foremost.
+                There's nothing I can't figure out with enough time and effort - which I have plenty of - then again not really. You want all this done before tomorrow's end and you waited til 4:30 to tell me about it? Sure, but I can't gaurantee the highest level of quality with a half hour today and only some time tomorrow.
+                You want me to stay late after work? - Hard no I have my own things I need to get done after work. Like getting a ticket out of this never ending hellscape.
+                I'm not one to constantly complain - but have some boundaries for yourselves out there. Employers love to 'lovebomb' - a term only recently found out about. If you know it - great if not google it.
+                You'll be waited on hand and foot only to be asked to break your back for a few dollars in comparison to the people running the show - when will employees have the upper hand in this toxicity?
+                The people who make all the money for a single entity. Without employees there's zero employers. Unless you're like me and find it important to be able to self sufficently do every aspect of the business all on your own.
+              </p>
+            </div>
+            <div className="orgo">
               <h2>Next Step</h2>
               <p>
                 Today I applied to one of the biggest tech companies of all time. It felt amazing - All thanks to my wonderful girlfriend, future wife, helping gather the support and materials to catapult me forward.
@@ -112,22 +111,22 @@ function Blog() {
                 It's about making that next step toward success when it's harshest that creates the best character.
                 Development is the never ending improvement whether it's in code or on the street.
               </p>
-              </div>
-              <div className="orgo">
-                <h2>Desperation Instead Patience</h2>
-                <p>
-                  When on the hunt one creates a certain instance to achieve that deems sucess. This instance takes patience instead of desperation to achieve maximum success.
-                  Don't count out the luck anyone can encounter during the process, but don't lean on it for sucess. 
-                  Patience is the idea of waiting for the opportune moment, rather than the waiting of success to happen on it's own.
-                  We simply cannot wait around for gold to find us, rather an active hunt for success. Sometimes we need peace and quiet to understand the reality of the situation.
-                  Inner focus is a variable for all and should not be slept on. Know when to distract from the goal so the goal chase can always be a fresh instance. Rituals of focus are neccesary to ensure sucess.
-                  There are billions of distractions; choose wisely. You could be sitting watching netflix for two hours or you could be out there learning a new skill for a new tomorrow.
-                  Find someone who loves you so much they're willing to change their entire life around so you can go be better. It doesn't fit all the niches of a ideal relationship, but ideal is for the bird; pick whatever works for you the best.
-                </p>
-              </div>
-              <div className="orgo">
-                <h2>Eye-Bee-Em</h2>
-                <p>
+            </div>
+            <div className="orgo">
+              <h2>Desperation Instead Patience</h2>
+              <p>
+                When on the hunt one creates a certain instance to achieve that deems sucess. This instance takes patience instead of desperation to achieve maximum success.
+                Don't count out the luck anyone can encounter during the process, but don't lean on it for sucess.
+                Patience is the idea of waiting for the opportune moment, rather than the waiting of success to happen on it's own.
+                We simply cannot wait around for gold to find us, rather an active hunt for success. Sometimes we need peace and quiet to understand the reality of the situation.
+                Inner focus is a variable for all and should not be slept on. Know when to distract from the goal so the goal chase can always be a fresh instance. Rituals of focus are neccesary to ensure sucess.
+                There are billions of distractions; choose wisely. You could be sitting watching netflix for two hours or you could be out there learning a new skill for a new tomorrow.
+                Find someone who loves you so much they're willing to change their entire life around so you can go be better. It doesn't fit all the niches of a ideal relationship, but ideal is for the bird; pick whatever works for you the best.
+              </p>
+            </div>
+            <div className="orgo">
+              <h2>Eye-Bee-Em</h2>
+              <p>
                 Yesterday the day consisted of absolute anxiousness followed by the biggest interview of my life to date then went on to be a relaxing day.
                 I don't know if I prepared enough, but I still managed to feel like a job well done. Either way of getting the new job or finding another route; I learned a great deal of information.
                 First, big companies care about their employees if they're doing it right - EyeBeeEm does that to a T. I've never applied or interviewed for a place that seems to take such fine care of the people who make their company propserous.
@@ -136,8 +135,32 @@ function Blog() {
                 They are willing to give me, and enforce professional development - what a catch! the company internally magnifies the skill force of all their employees and grants them the opportunity to put in and get out as much equally. NEVER happened to me ever before.
                 The biggest takeaway from this is that working at a place where you're at the bottom and the top at the same time is great for the first career job - after that go find something that builds your blocks up vertically instead of spreading thin across a never ending horizon.
                 It takes a great deal of courage to find a place and go for the gusto - it's absolutely worth it.
-                </p>
-              </div>
+              </p>
+            </div>
+            <div className="orgo">
+              <h2>Toxicity</h2>
+              <p>
+                Immediately following the previous blog I was brought in to my bosses office for an 'evaluation' which turned out to be a cornered situatino about whether or not I am pursuing other avenues for work.
+                I told them I had a phone screen on the previous Monday while withholding my interview experience the day prior. I was asked if there was anything they could do better - thinking to myself - <em>yes, you can find another graphic designer to help our workload</em> knowing that isn't going to happen.
+                They asked me about a suspsicius, undisclosed email they received about whether or not I was hired, I was confused as I only had one opportunity of different employment and they didnt contact my current employer beforehand.
+                Yeah so I lied about having an interview - only because I have zero clue whether or not that is even a feasible outcome. There's no point in warning the sheep about the wolf if the wolf isn't in sight.
+                I've never felt more uncomfortable at a job before in my life after that conversation happened - I was in borderline panic attack blackout happening mode. Ultimately, I couldn't care less about a company that is using me to make all their money for dimes to dollars.
+                There's a bigger, better fish for me to catch than whatever is going on now. That's why working for a company that is as desperate as you are to leave another company does not work out - refer to a previous post for further detail.
+              </p>
+            </div>
+            <div className="orgo">
+              <h2>Follow Up</h2>
+              <p>
+                I found the follow up letter from my interview with IBM and it gave me so much confidence to go forward. They actually took time to write the most extensive email I ever saw from a company, ever.
+                That simply shows how much they care about people - people that aren't even employed by them - and how much they appreciate someone's time and effort. I don't have a say in if they hire me or not, but perhaps they'll take a chance on me and I'll prove them right.
+                There's nothing much I can do now other than prepare to impress them next time I interact. I decided yesterday to pursue the cloud practitioner certification from AWS. Based solely on the recommendation from IBM. That is more help for me now than to be panicking on whatever else would stress me out.
+                It takes time to become great - not easily done rapidly - rather acheived only through time and struggle.
+                I find solace in knowing what to do instead of freaking out about what isn't done, yet. I applied to a load of other jobs throught this weekend and who knows if a fish will land. I know there's a great deal of support for my harships along the way. That's all I could ever really ask for in life - support from people who actually care about me.
+                There is not much more else that I could imagine doing than to get payed for coding - the whole graphic design career is seemingly dead to me. Working at small business locations expected to do 300% pace until the match is all burnt up is for the birds. I continue to ask for help of any sort and it's met with 'we're going to have to get this system down first then consider that option'
+                Whic is rough because that's basically saying free beer tomorrow at a bar.
+              </p>
+
+            </div>
           </div>
         </header>
       </div>
